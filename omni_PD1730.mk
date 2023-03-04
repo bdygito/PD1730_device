ff02171ff36ac975409fee2fde7a1d1c8db8d402
bdygito <126930735+bdygito@users.noreply.github.com>
2023-03-04 16:54:44 +0800

    Update and rename omni_msm8953_64.mk to omni_PD1730.mk

--- omni_PD1730.mk
+++ omni_PD1730.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from msm8953_64 device
+# Inherit from PD1730 device
 $(call inherit-product, device/vivo/PD1730/device.mk)
 
 PRODUCT_DEVICE := PD1730
@@ -24,6 +24,6 @@ PRODUCT_MANUFACTURER := vivo
 PRODUCT_GMS_CLIENTID_BASE := android-vivo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="msm8953_64-user 8.1.0 OPM1.171019.011 eng.compil.20190625.231648 release-keys"
+    PRIVATE_BUILD_DESC="PD1730-user 8.1.0 OPM1.171019.011 eng.compil.20190625.231648 release-keys"
 
 BUILD_FINGERPRINT := vivo/PD1730/PD1730:8.1.0/OPM1.171019.011/compil06252316:user/release-keys
